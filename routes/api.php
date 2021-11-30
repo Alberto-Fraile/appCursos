185e4b63ab5d90e87e86140ad140d74a6f0ad782
Alberto Fraile <albertofraile9@gmail.com>
2021-11-30 13:04:14 +0100

    ArregloApi

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\CursosController;
+use App\Http\Controllers\VideosController;
 
 /*
 |--------------------------------------------------------------------------
